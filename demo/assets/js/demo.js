30535d1eec95bd306cefeedc24c33a0c139f7bf0
yolo <kuroikenshi@sina.com>
2018-03-13 01:04:07 +0800

    Update demo to v0.8

---
+++ demo/assets/js/demo.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+	console.log('ready');
+});
+
+function go_index() {
+	location.href = 'demo-index.html';
+}
+function go_forum() {
+	location.href = 'demo-forum.html';
+}
+function go_exam() {
+	location.href = 'demo-exam.html';
+}
+
+function go_wrongList() {
+	location.href = 'demo-wrong-list.html';
+}
+function go_login() {
+	location.href = 'demo-login.html';
+}
+function go_personalConfig() {
+	location.href = 'demo-change-pwd.html'
+}
