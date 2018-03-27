e424b4f0189bde252561c2b5e80235c62a8a1b1d
yolo <kuroikenshi@sina.com>
2018-03-28 02:16:52 +0800

    Add login, use PerfectScrollbar

--- demo2/assets/js/demo.js
+++ demo2/assets/js/demo.js
@@ -1,33 +1,38 @@
 $(document).ready(function() {
 	console.log('ready');
+
+	new PerfectScrollbar('html');
 });
 
 //序列化表格元素为JSON  
-$.fn.serializeJson = function() {  
-    var o = {};  
-    var a = this.serializeArray();  
-    $.each(a, function() {  
-        if (o[this.name] !== undefined) {  
-            if (o[this.name] == null || !o[this.name].push) {  
-                o[this.name] = [o[this.name]];  
-            }  
-            o[this.name].push(this.value || null);  
-        } else {  
-            o[this.name] = this.value || '';  
-        }  
-    });  
-    return o;  
-}  
+$.fn.serializeJson = function() {
+	var o = {};
+	var a = this.serializeArray();
+	$.each(a, function() {
+		if (o[this.name] !== undefined) {
+			if (o[this.name] == null || !o[this.name].push) {
+				o[this.name] = [o[this.name]];
+			}
+			o[this.name].push(this.value || null);
+		} else {
+			o[this.name] = this.value || '';
+		}
+	});
+	return o;
+}
 
 function go_index() {
 	location.href = 'demo-index.html';
 }
+
 function go_forum() {
 	location.href = 'demo-forum.html';
 }
+
 function go_exam() {
 	location.href = 'demo-exam.html';
 }
+
 function go_examList() {
 	location.href = 'demo-exam-list.html';
 }
@@ -39,9 +44,11 @@ function go_result() {
 function go_wrongList() {
 	location.href = 'demo-wrong-list.html';
 }
+
 function go_login() {
 	location.href = 'demo-login.html';
 }
+
 function go_personalConfig() {
 	location.href = 'demo-change-pwd.html'
 }
@@ -52,8 +59,7 @@ function login(btn) {
 	console.log(data);
 	if (data.policeId == '123456' && data.pwd == '123456') {
 		go_index();
-	}
-	else {
+	} else {
 		alert('登陆失败！');
 	}
 }
